f0a07f868662ee7a7697abf437954019ff8eee68
Ronald Maciel <ronald.maciel@cwi.com.br>
2022-04-14 14:30:42 -0300

    add package unico

--- orb-onboarding/Package.swift
+++ orb-onboarding/Package.swift
@@ -5,23 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "orb-onboarding",
+    defaultLocalization: "pt-br",
+    platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "orb-onboarding",
             targets: ["orb-onboarding"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/acesso-io/unico-check-ios", from: "2.2.1"),
+        .package(name: "unicocheck-ios-spm", url: "https://github.com/acesso-io/unico-check-ios.git", from: "2.1.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "orb-onboarding",
-            dependencies: []),
+            dependencies: ["unicocheck-ios-spm"]),
         .testTarget(
             name: "orb-onboardingTests",
             dependencies: ["orb-onboarding"]),
